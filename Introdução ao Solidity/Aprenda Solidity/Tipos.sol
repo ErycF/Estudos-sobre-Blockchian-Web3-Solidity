4864ebe126ff4c33ea4069c8ef8fbafde0c3ab34
ErycF <erycfreire.pro@gmail.com>
2025-12-14 18:59:34 -0300

    Tipos

---
+++ Introdução ao Solidity/Aprenda Solidity/Tipos.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract Tipo{
+
+    // 0x, não é um numero, ele representa que a senquencia ira ser hexadessimal.
+
+    int152 public inteiro =78473;
+    uint248 public uinteiro = 10;
+    bytes2 public varbytes = 0x34f4; 
+    bytes3 public varbytes3 = 0x534573;
+    bool public boleana = false;
+    address public endereco = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
