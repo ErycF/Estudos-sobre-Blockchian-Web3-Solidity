5fe1ab55c711f6449d530f81b2a210af72acb046
ErycF <erycfreire.pro@gmail.com>
2025-12-14 18:12:56 -0300

    Retrno de função

--- Introdução ao Solidity/Aprenda Solidity/Retorno de funções.sol
+++ Introdução ao Solidity/Aprenda Solidity/Retorno de funções.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.30;
 contract Retorno{
 
     int contagem;
+    int contagem2;
     
-    function  AlteraContagem(int alteraNumero) public {
-        contagem = alteraNumero;
+    function  AlteraContagem(int alteraNumero1, int alteraNumero2) public {
+        contagem = alteraNumero1;
+        contagem2 = alteraNumero2;
     }
     function retornaComtagem1() public view returns (int){
         return contagem; // returns ([tipo])
@@ -15,6 +17,6 @@ contract Retorno{
         retorna = contagem; // returns ([Tipo] Nome)
     }
     function reotrnaDuasContagem3() public view returns (int, int){
-        return (contagem, contagem);
+        return (contagem, contagem2);
     }
 }
