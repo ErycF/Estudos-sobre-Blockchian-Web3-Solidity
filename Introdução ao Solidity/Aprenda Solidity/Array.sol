1917e91741fa0b77dd96956b59b3a42b4666ba03
ErycF <erycfreire.pro@gmail.com>
2025-12-16 23:06:19 -0300

    Inicializando arrays

---
+++ Introdução ao Solidity/Aprenda Solidity/Array.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract Arrays{
+
+    uint [3] public MeuNumero = [1,2,3];
+    string [3] public MinhaPalavra =  ["Um", "Dois", ""];
+
+    uint[] public dimanico = new uint[](10);
+    // Irar inicializar a ARRAY com 10 elementos todoos eles no valor padrão.
+}
