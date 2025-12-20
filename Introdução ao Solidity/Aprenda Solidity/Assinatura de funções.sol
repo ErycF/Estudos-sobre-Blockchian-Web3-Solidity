e072eb31676bc7c444aca7adebde77362c2aac5a
ErycF <erycfreire.pro@gmail.com>
2025-12-20 15:39:36 -0300

    Assinatura de funções 2/2
    como calcular a assinatura de funções com múltiplos parâmetros.

--- Introdução ao Solidity/Aprenda Solidity/Assinatura de funções.sol
+++ Introdução ao Solidity/Aprenda Solidity/Assinatura de funções.sol
@@ -14,8 +14,20 @@ contract assinatura{
     function alteraNumero(uint _numero) public {
         numero = _numero;
     }
-    /*
+
+    // 7c141f0a
+    function Soma(uint _x, uint _y) public pure returns (uint, bytes4){
+        return (_x + _y, msg.sig);
+    }
+
+    // 29803b21
+    function join(string memory first, string memory last) public pure returns (string memory, bytes4){
+        return (string.concat(first, last), msg.sig);
+    }
+/*
     A assinatura são os primeiros 4 bytes do hash do NOME DA FUNÇÃO -> pegarNumero() = 44b6f21c
     NOME DA FUNÇÃO -> alteraNumero(uint) = 2bc97773
-    */
-}
+
+    O MSG.SIG ele pega a assinatura da função que são os primeiros 4 bytes
+*/
+}    
