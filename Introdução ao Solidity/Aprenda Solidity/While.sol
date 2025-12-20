c155a9087d77818d31bb6c7ae0c67b0909a09e63
ErycF <erycfreire.pro@gmail.com>
2025-12-20 13:15:03 -0300

    Break e continue
    Aprender sobre os comandos break e continue, que podemos utilizar dentro de laços.

--- Introdução ao Solidity/Aprenda Solidity/While.sol
+++ Introdução ao Solidity/Aprenda Solidity/While.sol
@@ -8,12 +8,46 @@ contract While{
         uint soma;
         uint contagem;
 
-        while (contagem < 10){
+        while (contagem <= 10){
             
             contagem++;
             soma = contagem + soma;
         }
         return soma;
+    }
+
+    function repetirBreak() public pure returns (uint, uint){
+
+        uint soma;
+        uint contagem;
+
+        while (contagem <= 10){
+            contagem++;
+
+            if(contagem == 5){
+                break;
+                // Quandoo chegar no BREAK, ele (SAiR DO LAÇO), e vai para a função
+            }
+            soma++;
+        }
+        return (soma, contagem);
+    }
+
+    function repetirContinue() public pure returns (uint, uint){
+
+        uint soma;
+        uint contagem;
+
+        while (contagem <= 10){
+            contagem++;
+
+            if (contagem == 5){
+                continue;
+                // O CONTINUE ele vai continuar no sistema de repetição, porem sem entra no IF
+            }
+            soma++;
+        }
+        return (soma, contagem);
     }
 }
 /*
