b68907b0228544197f31b6199b24ea40f136569e
ErycF <erycfreire.pro@gmail.com>
2025-12-21 19:52:08 -0300

    Encode Packed
    Como utilizar o método abi.encodePacked em Solidity.

--- Introdução ao Solidity/Aprenda Solidity/ABI.encode.sol
+++ Introdução ao Solidity/Aprenda Solidity/ABI.encode.sol
@@ -14,13 +14,21 @@ contract Encode{
     function decode(bytes calldata _valor) public pure returns (uint, uint, string memory, bool){
         return abi.decode(_valor, (uint, uint, string, bool));
     }
+
+    function encodePacote() public pure returns (bytes memory){
+        return abi.encodePacked(uint8 (3), uint16 (7), string ("Eryc"));
+    }
+
+    function concatena(string memory s1, string memory s2) public pure returns (string memory){
+        return string(abi.encodePacked(s1, s2));
+    }
 }
 
 /*
-    O encode ele irar codificar o parametro que você colocar;
-    O encodeSignature ele irar codificar pore3m ele trara a assinatura.
-    E o decode ele irar fazer o inverso do encode 
-    
-    
-    
+    O encode ele irar codificar o parametro que você colocar => abi.encode(2025, 20, "Eryc", true);
+    O encodeSignature ele irar codificar porem ele trará a assinatura;
+    E o decode ele irar fazer o inverso do encode.
+
+    O abi.encodePacked ele pega os parametros e concatena o byteCode dos parametros;
+    O string(abi.encodePacked(s1, s2)), ele passa parametros do tipo string, e junta elas.
 */
