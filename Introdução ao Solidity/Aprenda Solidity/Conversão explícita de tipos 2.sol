ea6eca0bb16a76e6b6cce33566a5629380beadaa
ErycF <erycfreire.pro@gmail.com>
2025-12-23 19:40:15 -0300

    Conversão explícita de tipos 2/2
    Como fazer conversões explícitas de tipos em Solidity.

---
+++ Introdução ao Solidity/Aprenda Solidity/Conversão explícita de tipos 2.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract Explicita{
+
+    function converte() public pure {
+
+        /*uint8[4] memory array = [uint8(1),2,3,4]; 
+        Precisamos converte o primeiro elemento para ele saber que, queremos os números em uint8;*/
+    }
+
+        function converte2() public pure {
+            
+            //address endereco = payable address(this);
+            //payable(endereco).transfer;
+
+    }
+}
+
+/*
+    CONVERSÃO EXPLÍCITA
+
+    address(this);
+    payable([address]);
+    uint[] memory arrray = [uint(1),...];
+*/
