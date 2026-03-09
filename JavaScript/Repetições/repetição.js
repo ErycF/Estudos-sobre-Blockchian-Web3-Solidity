6b8404b990fd886512647508a59cd20c3752e345
Eryc <erycfreire.pro@gmail.com>
2026-03-09 16:34:19 -0300

    Repetições (Parte 1)
    Curso JavaScript

---
+++ JavaScript/Repetições/repetição.js
@@ -0,0 +1,16 @@
+// Estrutura de repetição de laço
+var i = 1
+
+while(i <= 10){
+    console.log(`Repetição do tipo laço: ${i}`)
+    i++
+}
+
+i = 1 
+console.log('---------------------------')
+
+do {
+    console.log(`Repetição do tipo Do: ${i}`)
+    i++
+
+}while(i <= 10)
