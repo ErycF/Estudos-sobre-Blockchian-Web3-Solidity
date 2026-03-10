a5f941b8c1ba6868466f8da378791c7e99477098
Eryc <erycfreire.pro@gmail.com>
2026-03-10 19:26:31 -0300

    Exercícios Tabuada
    Curso JavaScript

--- JavaScript/Repetições/Exercícios JavaScript/Exercício Tabuada/script.js
+++ JavaScript/Repetições/Exercícios JavaScript/Exercício Tabuada/script.js
@@ -1,3 +1,21 @@
-function run(){
-    
+function tabuada(){
+    let nun = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if(nun.value.length == 0){
+        alert('Por favor, digite um número')
+    }else {
+
+        let n = Number(nun.value)
+        let i = 1
+
+        tab.innerHTML = ''
+        while(i <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} * ${i} = ${n*i}`
+            item.value = `tab${i}`
+            tab.appendChild(item)
+            i++
+        }
+    }
 }
