ea54df94f615694adc14d48a0553d6cb2245b4a5
Eryc <erycfreire.pro@gmail.com>
2026-03-12 13:52:13 -0300

    Analisando Valor (Parte 3)
     Curso JavaScript

--- JavaScript/Funções/Exercícios JavaScript/script.js
+++ JavaScript/Funções/Exercícios JavaScript/script.js
@@ -24,8 +24,47 @@ function adicionar(){
     let num = Number(caixa.value)
 
     if(isNumero(num) && !inLista(num, valores)){
-        alert('tudo ok')
+        valores.push(num)
+        let item = document.createElement('option')
+        item.text = `Valor adicionado: ${num}`
+        lista.appendChild(item)
     }else{
         alert('Valor invalido ou já encontrado na lista')
+    }
+
+    num = ''
+    num.focus()
+}
+
+function finalizar(){
+
+    if(valores.length == 0 ){
+        alert('Adicione valores antes de finalizar')
+    }else{
+        let lot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+        for(let i in valores){
+            soma += valores[i]
+
+            if(valores[i] > maior){
+                maior = valores[i]
+
+            } else if(valores[i] < menor){
+                menor = valores[i]
+
+            }
+        }
+        media = soma / lot
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos <strong>${lot}</strong> números cadastrado</p>`
+        res.innerHTML += `<p>O maior número informado foi: <strong>${maior}</strong></p>`
+        res.innerHTML += `<p>O menor número informado foi: <strong>${menor}</strong></p>`
+        res.innerHTML += `<p>Somando todos os valores: <strong>${soma}</strong></p>`
+        res.innerHTML += `<p>A media dos valores digitados são: <strong>${media}</strong></p>`
     }
 }
