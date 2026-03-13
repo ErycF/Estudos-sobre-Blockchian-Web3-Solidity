819331cfa5e16c8928df9853b66f56c7cb3062f9
Eryc <erycfreire.pro@gmail.com>
2026-03-13 20:11:32 -0300

    Valores  e Tipos
    Valores e Tipos em JavaScript

---
+++ JavaScript/Aprenda JavaScript /Valores e tipos/valoresTipos.js
@@ -0,0 +1,28 @@
+// Valores e Tipos
+
+// Number = Comporta  qualquer número seja decimal, inteiro, negativo, ou notação cientifica; tudo amarvenada dentro de 64 bits que é 8 Bytes;
+
+// String = Uma string é um conjunto de caracteres, em unicode, codificado para UTF-16;
+
+// Boolean = Verdadeiro ou falso; true or false;
+
+// Undefined e null = Undefined e null indicam a ausẽncia de valor. Em geral, underfined é gerado pelo JavaScript para indicar um valor não inicializado, enquando que null é ultilizado pelo programador;
+
+// BigInt e Symbol = BigInt denota grande inteiros, e Symbol é um tipo que armazena um valor único;
+
+
+let Number = [12, -8, 1.77, 1.34e3]
+
+let string = ['Moto', "Carro", `Casa`]
+
+let Boolean = [true, false]
+
+let semValor = [undefined, null]
+
+console.log(`Número: Intero ${Number[0]}, Negativo ${Number[1]}, Decinal ${Number[2]}`)
+console.log(`String: ${string[0]}, ${string[1]}, ${string[2]}`)
+console.log(`Boolean: Verdadeiro ${Boolean[0]}, Falso ${Boolean[1]}`)
+console.log(`Sem Valores: Undefined ${semValor[0]}, Null ${semValor[1]}`)
+
+
+
