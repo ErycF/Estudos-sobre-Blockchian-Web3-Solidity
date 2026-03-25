0566c4b7d02074815268f36afa4c0f1c11d139b4
Eryc <erycfreire.pro@gmail.com>
2026-03-25 15:22:55 -0300

    string

---
+++ JavaScript/Aprenda JavaScript /String/string.js
@@ -0,0 +1,16 @@
+
+const emoji = '♟';
+
+let texto = 'Eryc';
+
+let texto2 = 'Anão';
+
+let texto3 = 'Ola,/n esse texto/n quebra ';
+
+console.log(emoji, emoji.length, emoji[0]);
+
+console.log(texto[0]);
+
+console.log(texto2[2], texto2.length);
+
+console.log(texto3)
